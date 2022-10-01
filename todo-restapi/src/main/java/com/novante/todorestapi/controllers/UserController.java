7a39b85dacf017bb4ce7a571851202c0c04c3cdc
NovaAnte <andreas_cabraja@hotmail.com>
2022-10-01 17:57:08 +0200

    Added PATCH-method for updating User.

--- todo-restapi/src/main/java/com/novante/todorestapi/controllers/UserController.java
+++ todo-restapi/src/main/java/com/novante/todorestapi/controllers/UserController.java
@@ -2,10 +2,9 @@ package com.novante.todorestapi.controllers;
 
 import com.novante.todorestapi.entities.User;
 import com.novante.todorestapi.services.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -22,6 +21,19 @@ public class UserController {
         return userService.getUserById(userId);
     }
 
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
 
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PatchMapping("/userId/{userId}")
+    public User updateUser(@RequestBody User user, @PathVariable Long userId) {
+        return userService.updateUser(user, userId);
+    }
 
 }
--- todo-restapi/src/main/java/com/novante/todorestapi/services/UserService.java
+++ todo-restapi/src/main/java/com/novante/todorestapi/services/UserService.java
@@ -4,6 +4,7 @@ import com.novante.todorestapi.entities.User;
 import com.novante.todorestapi.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,5 +20,32 @@ public class UserService {
     public User getUserById(Long userId) {
         Optional<User> foundUser = userRepository.findById(userId);
         return foundUser.get();
+    }
+
+    public List<User> getAllUsers() {
+        List<User> foundUsers = userRepository.findAll();
+        return foundUsers;
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(User user, Long userId) {
+        User foundUser = userRepository.findById(userId).get();
+
+        if (user.getUsername() != null){
+            foundUser.setUsername(user.getUsername());
+        }
+
+        if (user.getPassword() != null){
+            foundUser.setPassword(user.getPassword());
+        }
+
+        if (user.getTodoLists() != null){
+            foundUser.setTodoLists(user.getTodoLists());
+        }
+
+        return userRepository.save(foundUser);
     }
 }
