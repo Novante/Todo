73c18038196d043ad9fd9cb8996c57ba7bcdaf1c
NovaAnte <andreas_cabraja@hotmail.com>
2022-10-01 20:42:22 +0200

    Scrapped Ant Design, making my own Todo-table instead.

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -7,88 +7,7 @@ import axios from "axios";
 
 const Homepage = () => {
 
-    const EditableContext = React.createContext(null);
-    const EditableRow = ({index, ...props}) => {
-        const [form] = Form.useForm();
-        return (
-            <Form form={form} component={false}>
-                <EditableContext.Provider value={form}>
-                    <tr {...props} />
-                </EditableContext.Provider>
-            </Form>
-        );
-    };
-
-    const EditableCell = ({
-                              title,
-                              editable,
-                              children,
-                              dataIndex,
-                              record,
-                              handleSave,
-                              ...restProps
-                          }) => {
-        const [editing, setEditing] = useState(false);
-        const inputRef = useRef(null);
-        const form = useContext(EditableContext);
-        useEffect(() => {
-            if (editing) {
-                inputRef.current.focus();
-            }
-        }, [editing]);
-
-        const toggleEdit = () => {
-            setEditing(!editing);
-            form.setFieldsValue({
-                [dataIndex]: record[dataIndex],
-            });
-        };
-
-        const save = async () => {
-            try {
-                const values = await form.validateFields();
-                toggleEdit();
-                handleSave({...record, ...values});
-            } catch (errInfo) {
-                console.log('Save failed:', errInfo);
-            }
-        };
-
-        let childNode = children;
-
-        if (editable) {
-            childNode = editing ? (
-                <Form.Item
-                    style={{
-                        margin: 0,
-                    }}
-                    name={dataIndex}
-                    rules={[
-                        {
-                            required: true,
-                            message: `${title} krävs.`,
-                        },
-                    ]}
-                >
-                    <Input ref={inputRef} onPressEnter={save} onBlur={save}/>
-                </Form.Item>
-            ) : (
-                <div
-                    className="editable-cell-value-wrap"
-                    style={{
-                        paddingRight: 24,
-                    }}
-                    onClick={toggleEdit}
-                >
-                    {children}
-                </div>
-            );
-        }
-
-        return <td {...restProps}>{childNode}</td>;
-    };
-
-    const [dataSource, setDataSource] = useState()
+    const [dataSource, setDataSource] = useState([])
 
     useEffect(() => {
         fetchTodos()
@@ -99,102 +18,34 @@ const Homepage = () => {
         setDataSource(data.data)
     }
 
-    const [count, setCount] = useState(2);
-
-    const handleDelete = (key) => {
-        const newData = dataSource.filter((item) => item.key !== key);
-        setDataSource(newData);
-    };
-
-    const defaultColumns = [
-        {
-            title: 'Rubrik',
-            dataIndex: 'todoName',
-            width: '30%',
-            editable: true,
-        },
-        {
-            title: 'Innehåll',
-            dataIndex: 'todoContent',
-            editable: true
-        },
-        // {
-        //     title: 'address',
-        //     dataIndex: 'address',
-        // },
-        {
-            title: 'operation',
-            dataIndex: 'operation',
-            render: (_, record) =>
-                dataSource.length >= 1 ? (
-                    <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(record.key)}>
-                        <a>Delete</a>
-                    </Popconfirm>
-                ) : null,
-        },
-    ];
-
-    const handleAdd = () => {
-        const newData = {
-            key: count,
-            todoName: `Namn`,
-            todoContent: 'Innehåll',
-        };
-        setDataSource([...dataSource, newData]);
-        setCount(count + 1);
-    };
-
-    const handleSave = (row) => {
-        const newData = [...dataSource];
-        const index = newData.findIndex((item) => row.key === item.key);
-        const item = newData[index];
-        newData.splice(index, 1, {...item, ...row});
-        setDataSource(newData);
-    };
-
-    const components = {
-        body: {
-            row: EditableRow,
-            cell: EditableCell,
-        },
-    };
-    const columns = defaultColumns.map((col) => {
-        if (!col.editable) {
-            return col;
-        }
-
-
-        return {
-            ...col,
-            onCell: (record) => ({
-                record,
-                editable: col.editable,
-                dataIndex: col.dataIndex,
-                title: col.title,
-                handleSave,
-            }),
-        };
-    });
     return (
         <div>
-            <Button
-                onClick={handleAdd}
-                type="primary"
-                style={{
-                    marginBottom: 16,
-                }}
-            >
-                Add a row
-            </Button>
-            <Table
-                components={components}
-                rowClassName={() => 'editable-row'}
-                bordered
-                dataSource={dataSource}
-                columns={columns}
-            />
+            <div style={{display: 'flex', justifyContent: "center", textAlign: 'left'}}>
+                <table style={{width: '50%', ...styles.box}}>
+                    <tr style={styles.box}>
+                        <th style={styles.box}>Titel</th>
+                        <th style={styles.box}>Beskrivning</th>
+                    </tr>
+                    {dataSource.map((dataSource) => {
+                        return (
+                            <tr>
+                                <td style={styles.box}>{dataSource.todoName}</td>
+                                <td style={styles.box}>{dataSource.todoContent}</td>
+                            </tr>
+                        )
+                    })}
+
+                </table>
+            </div>
         </div>
-    );
+    )
+
+}
+
+const styles = {
+    box: {
+        border: '1px solid black'
+    }
 }
 
 export default Homepage;
