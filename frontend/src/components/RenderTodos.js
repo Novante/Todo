13c7b85cb293f6dd306ee4823d09287df6dbf9a6
NovaAnte <andreas_cabraja@hotmail.com>
2022-10-02 02:19:31 +0200

    Possible to modify both todoName and todoContent in the webapp.

--- frontend/src/components/RenderTodos.js
+++ frontend/src/components/RenderTodos.js
@@ -2,18 +2,22 @@ import React, {useEffect, useState} from "react";
 import {dataApi} from "../Api";
 import axios from "axios";
 
-const RenderTodos = ({dataSource, setDataSource, key, id}) => {
+const RenderTodos = ({dataSource, id}) => {
     const [editMode, setEditMode] = useState(false)
     const [editTitle, setEditTitle] = useState(dataSource.todoName)
     const [editContent, setEditContent] = useState(dataSource.todoContent)
-
-    console.log(id)
 
     const changeData = async () => {
         if (editTitle !== dataSource.todoName) {
             await axios.patch(`http://localhost:8080/api/todoitem/userId/1`, {
                 todoId: id,
                 todoName: editTitle
+            })
+
+        } if (editContent !== dataSource.todoContent){
+            await axios.patch(`http://localhost:8080/api/todoitem/userId/1`, {
+                todoId: id,
+                todoContent: editContent
             })
         }
     }
@@ -22,10 +26,19 @@ const RenderTodos = ({dataSource, setDataSource, key, id}) => {
         <tr style={styles.box}>
             {
                 editMode === true ?
-                    <input type="text" onChange={e => setEditTitle(e.target.value)} style={styles.editMode} value={editTitle}></input> :
+                   <td> <input type="text" onChange={e => setEditTitle(e.target.value)} style={styles.editMode} value={editTitle}></input></td>
+                    :
                     <td style={{paddingLeft: '4px'}}>{editTitle}</td>
             }
-            <td style={{...styles.box, lineHeight: '50px', padding: 0, paddingLeft: '4px'}}>{dataSource.todoContent}</td>
+
+            {
+                editMode === true ?
+                    <td><input type="text" onChange={e => setEditContent(e.target.value)} style={styles.editMode} value={editContent}></input></td>
+                        :
+                    <td style={{...styles.box, lineHeight: '50px', padding: 0, paddingLeft: '4px'}}>{editContent}</td>
+
+            }
+
 {
                 editMode === false ?
                     <button onClick={() => setEditMode(!editMode)}
